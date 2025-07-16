a163d71e76aa213feed9644c7cc2d177f6e94cb2
levimhuillet <levimhuillet@gmail.com>
2025-07-16 16:35:54 -0500

    enable aspect clamping

---
+++ Assets/Core/Code/SpaceFabGame.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using BeauPools;
+using BeauRoutine;
+using BeauUtil;
+using BeauUtil.Debugger;
+using FieldDay;
+using FieldDay.UI.Animation;
+using UnityEngine;
+
+[assembly: InternalsVisibleTo("Assembly-CSharp-Editor")]
+
+namespace SpaceFab
+{
+    public sealed class SpaceFabGame : Game
+    {
+
+        [InvokePreBoot]
+        static private void OnPreBoot()
+        {
+            Rendering.EnableAspectClamping(4, 3);
+        }
+    }
+}
