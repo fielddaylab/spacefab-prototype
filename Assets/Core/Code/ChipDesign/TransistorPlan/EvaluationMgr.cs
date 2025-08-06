39dba46c8b0286a2aaf5d2dcc58bbc44160f0159
levimhuillet <levimhuillet@gmail.com>
2025-08-06 12:35:25 -0500

    cost summary

--- Assets/Core/Code/ChipDesign/TransistorPlan/EvaluationMgr.cs
+++ Assets/Core/Code/ChipDesign/TransistorPlan/EvaluationMgr.cs
@@ -20,7 +20,18 @@ namespace SpaceFab.ChipDesign
         [SerializeField] private TMP_Text EvaluatePanelTitleText;
         [SerializeField] private Button ReviseButton;
         [SerializeField] private Button ContinueButton;
+        [SerializeField] private TMP_Text UsedSpaceText;
+        [SerializeField] private TMP_Text SpaceCostText;
+        [SerializeField] private TMP_Text NodeCostText;
+        [SerializeField] private TMP_Text LinkCostText;
+        [SerializeField] private TMP_Text ValueText;
+        [SerializeField] private TMP_Text ProfitText;
 
+        [Header("Costs")]
+        public float CostPerLinkUnit;
+        public float CostPerNode;
+        public float CostPerSpace;
+        public float ProjectValue;
 
         private OutputNode m_OutNode;
 
@@ -140,10 +151,143 @@ namespace SpaceFab.ChipDesign
             if (success) { EvaluateText.SetText("Correct!"); }
             else { EvaluateText.SetText("Incorrect"); }
 
+            bool profitable = UpdateCostBreakdown();
+
             EvaluatePanel.SetActive(true);
-            if (success) { EvaluatePanelTitleText.SetText("Design Validated!"); }
+            if (profitable && success) { EvaluatePanelTitleText.SetText("Design Validated!"); }
             else { EvaluatePanelTitleText.SetText("Back to the\nDrawing Board..."); }
-            ContinueButton.interactable = success;
+            ContinueButton.interactable = success && profitable;
+        }
+
+        private bool UpdateCostBreakdown()
+        {
+            // space cost
+            int numUnits = CalcNumSpaceUnits();
+            float spaceCost = CostPerSpace * numUnits;
+
+            // transistor cost
+            int numTransistorNodes = 0;
+            var allNodes = InteractionMgr.Instance.GetAllNodes();
+            foreach (var node in allNodes)
+            {
+                if (node.NodeType == NodeType.N || node.NodeType == NodeType.P)
+                {
+                    numTransistorNodes++;
+                }
+            }
+            float transistorCost = numTransistorNodes * CostPerNode;
+
+            // link cost
+            float linkCost = 0;
+            var allLinks = InteractionMgr.Instance.GetAllLinks();
+            foreach (var links in allLinks)
+            {
+                var dist = Vector3.Distance(links.EndAnchor.transform.position, links.StartAnchor.transform.position);
+                linkCost += dist * CostPerLinkUnit;
+            }
+
+            float profit = ProjectValue - linkCost - transistorCost - spaceCost;
+
+            // set text
+            UsedSpaceText.SetText(numUnits + " nm^2");
+            SpaceCostText.SetText("$" + spaceCost.ToString("0.00"));
+            SpaceCostText.color = Color.red;
+            NodeCostText.SetText("$" + transistorCost.ToString("0.00"));
+            NodeCostText.color = Color.red;
+            LinkCostText.SetText("$" + linkCost.ToString("0.00"));
+            LinkCostText.color = Color.red;
+            ValueText.SetText("$" + ProjectValue.ToString("0.00"));
+            ValueText.color = Color.green;
+            ProfitText.SetText("$" + profit.ToString("0.00"));
+            ProfitText.color = profit > 0 ? Color.green : Color.red;
+
+            return profit > 0;
+        }
+
+        private int CalcNumSpaceUnits()
+        {
+            bool anyNodes = false;
+
+            int yBounds = 6;
+            int xBounds = 8;
+
+            int topY = yBounds;
+            bool hitAny = false;
+
+            var yBox = new Vector2(xBounds * 2, 0.5f);
+            var xBox = new Vector2(0.5f, yBounds * 2);
+
+            while (!hitAny && topY >= -yBounds)
+            {
+                Vector2 currPos = new Vector2(0, topY);
+                Collider2D hit = Physics2D.OverlapBox(currPos, yBox, 0, 1 << LayerMask.NameToLayer("Nodes"));
+                if (hit != null)
+                {
+                    hitAny = true;
+                    anyNodes = true;
+                    break;
+                }
+                
+                topY--;
+            }
+
+            hitAny = false;
+            int bottomY = -yBounds;
+            while (!hitAny && bottomY <= topY)
+            {
+                Vector2 currPos = new Vector2(0, bottomY);
+                Collider2D hit = Physics2D.OverlapBox(currPos, yBox, 0, 1 << LayerMask.NameToLayer("Nodes"));
+                if (hit != null)
+                {
+                    hitAny = true;
+                    anyNodes = true;
+                    break;
+                }
+
+                bottomY++;
+            }
+
+            int leftX = -xBounds;
+            hitAny = false;
+            while (!hitAny && leftX <= xBounds)
+            {
+                Vector2 currPos = new Vector2(leftX, 0);
+                Collider2D hit = Physics2D.OverlapBox(currPos, xBox, 0, 1 << LayerMask.NameToLayer("Nodes"));
+                if (hit != null)
+                {
+                    hitAny = true;
+                    anyNodes = true;
+                    break;
+                }
+
+                leftX++;
+            }
+
+            int rightX = xBounds;
+            hitAny = false;
+            while (!hitAny && rightX >= leftX)
+            {
+                Vector2 currPos = new Vector2(rightX, 0);
+                Collider2D hit = Physics2D.OverlapBox(currPos, xBox, 0, 1 << LayerMask.NameToLayer("Nodes"));
+                if (hit != null)
+                {
+                    hitAny = true;
+                    anyNodes = true;
+                    break;
+                }
+
+                rightX--;
+            }
+
+            int height = topY - bottomY;
+            int width = rightX - leftX;
+            if (anyNodes)
+            {
+                height++;
+                width++;
+            }
+
+            return width * height;
         }
 
         #endregion // Evaluation
