3b2a85f9b148c7ab151f898654bdb94fab20d4aa
levimhuillet <levimhuillet@gmail.com>
2025-09-11 13:11:16 -0500

    setup sputtering and etch

--- Assets/Core/Code/ChipFab/Microgames/EtchMicrogame.cs
+++ Assets/Core/Code/ChipFab/Microgames/EtchMicrogame.cs
@@ -4,16 +4,67 @@ using UnityEngine;
 
 namespace SpaceFab.ChipFab
 {
+    public enum EtchMicrogameState
+    {
+        Activated,
+        Ready,
+        Etching,
+        Finished,
+        Deactivated
+    }
+
     public class EtchMicrogame : StationMicrogame, IStationMicrogame
     {
+
+        private EtchMicrogameState m_state;
+
+        #region IStationMicrogame
+
         public override void Activate(WaferState waferState)
         {
             base.Activate(waferState);
+
+            TransitionToActivated();
         }
 
         public override void Deactivate()
         {
             base.Deactivate();
+        }
+
+        #endregion // IStationMicrogame
+
+
+        private void Update()
+        {
+            switch (m_state)
+            {
+                case EtchMicrogameState.Activated:
+                    // TransitionToReady();
+                    break;
+                case EtchMicrogameState.Ready:
+                    // TransitionToSpinning();
+                    break;
+                case EtchMicrogameState.Etching:
+                    // ProcessMicrogame();
+                    break;
+                case EtchMicrogameState.Finished:
+                    // Deactivate();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void TransitionToActivated()
+        {
+            m_state = EtchMicrogameState.Activated;
+            TransitionCommon();
+        }
+
+        private void TransitionCommon()
+        {
+
         }
     }
 }
--- Assets/Core/Code/ChipFab/Microgames/SputteringMicrogame.cs
+++ Assets/Core/Code/ChipFab/Microgames/SputteringMicrogame.cs
@@ -4,16 +4,65 @@ using UnityEngine;
 
 namespace SpaceFab.ChipFab
 {
+    public enum SputteringMicrogameState
+    {
+        Activated,
+        Ready,
+        Sputtering,
+        Finished,
+        Deactivated
+    }
+
     public class SputteringMicrogame : StationMicrogame, IStationMicrogame
     {
+        private SputteringMicrogameState m_state;
+
+        #region IStationMicrogame
+
         public override void Activate(WaferState waferState)
         {
             base.Activate(waferState);
+
+            TransitionToActivated();
         }
 
         public override void Deactivate()
         {
             base.Deactivate();
+        }
+
+        #endregion // IStationMicrogame
+
+        private void Update()
+        {
+            switch (m_state)
+            {
+                case SputteringMicrogameState.Activated:
+                    // TransitionToReady();
+                    break;
+                case SputteringMicrogameState.Ready:
+                    // TransitionToSpinning();
+                    break;
+                case SputteringMicrogameState.Sputtering:
+                    // ProcessMicrogame();
+                    break;
+                case SputteringMicrogameState.Finished:
+                    // Deactivate();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void TransitionToActivated()
+        {
+            m_state = SputteringMicrogameState.Activated;
+            TransitionCommon();
+        }
+
+        private void TransitionCommon()
+        {
+
         }
     }
 }
