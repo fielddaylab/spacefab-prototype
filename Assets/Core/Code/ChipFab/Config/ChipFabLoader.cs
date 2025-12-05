2e174acb14f68457dbaad1b02907575f1986e894
levimhuillet <levimhuillet@gmail.com>
2025-12-05 11:59:40 -0600

    stub levels

--- Assets/Core/Code/ChipFab/Config/ChipFabLoader.cs
+++ Assets/Core/Code/ChipFab/Config/ChipFabLoader.cs
@@ -8,60 +8,9 @@ namespace SpaceFab.ChipFab
 {
     public class ChipFabLoader : MonoBehaviour
     {
-        public Button CozyModeBtn;
-        public Button TimedModeBtn;
-
-        public Button NoAutoBtn;
-        public Button HybridAutoBtn;
-        public Button AutoBtn;
-
-        public Button StartBtn;
-        [SerializeField] private string m_ChipFabScene;
-
-        [SerializeField] private LevelSetupData NoAutoData;
-        [SerializeField] private LevelSetupData HybridAutoData;
-        [SerializeField] private LevelSetupData AutoData;
-
         private void Awake()
         {
-            CozyModeBtn.onClick.AddListener(HandleCozyClicked);
-            TimedModeBtn.onClick.AddListener(HandleTimeClicked);
-
-            NoAutoBtn.onClick.AddListener(HandleNoAutoClicked);
-            HybridAutoBtn.onClick.AddListener(HandleHybridAutoClicked);
-            AutoBtn.onClick.AddListener(HandleAutoClicked);
-
-            StartBtn.onClick.AddListener(HandleStartClicked);
-        }
-
-        private void HandleStartClicked()
-        {
-            SceneManager.LoadScene(m_ChipFabScene);
-        }
-
-        private void HandleCozyClicked()
-        {
-            ChipFabConfig.Instance.Mode = GameMode.Cozy;
-        }
-
-        private void HandleTimeClicked()
-        {
             ChipFabConfig.Instance.Mode = GameMode.Timed;
-        }
-
-        private void HandleNoAutoClicked()
-        {
-            ChipFabConfig.Instance.CurrLevel = NoAutoData;
-        }
-
-        private void HandleHybridAutoClicked()
-        {
-            ChipFabConfig.Instance.CurrLevel = HybridAutoData;
-        }
-
-        private void HandleAutoClicked()
-        {
-            ChipFabConfig.Instance.CurrLevel = AutoData;
         }
     }
 }
